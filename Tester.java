455edbd60bdf6ce3d9911f4205d190528b67638f
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:23:32 +0200

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -2,5 +2,9 @@ public class Tester
 {
     public static void main(String[] args)
     {
+        WordGames wg = new WordGames();
+
+        System.out.println(wg.addHelloWord("World!"));
+        System.out.println(wg.getFullName("Enrico", "Drago"));
     }
 }
