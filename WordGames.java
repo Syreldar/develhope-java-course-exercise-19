f2a5878bf84282f1eb1e74f07be33fe29e937869
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:21:31 +0200

    Update WordGames.java

--- WordGames.java
+++ WordGames.java
@@ -1,3 +1,12 @@
 public class WordGames
 {
+    public String addHelloWord(String word)
+    {
+        return String.format("Hello %s", word);
+    }
+
+    public String getFullName(String name, String surname)
+    {
+        return name + " " + surname;
+    }
 }
