3682af5b97cbf531125831ad2c6ceb5382535681
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:23:49 +0200

    Added javadoc to Tester,java

--- Tester.java
+++ Tester.java
@@ -1,5 +1,15 @@
+/**
+ * Tests methods in the WordGames class.
+ *
+ * @author Enrico Drago
+ */
 public class Tester
 {
+    /**
+     * The main method in the Tester class for testing the WordGames class.
+     *
+     * @param args Unused.
+     */
     public static void main(String[] args)
     {
         WordGames wg = new WordGames();
