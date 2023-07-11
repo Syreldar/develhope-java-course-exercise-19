fd6969d5443065ca973e11f779872558f2b9593a
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:21:56 +0200

    Added javadoc to WordGames.java

--- WordGames.java
+++ WordGames.java
@@ -1,10 +1,28 @@
+/**
+ * provides methods for various word-based games.
+ *
+ * @author Enrico Drago
+ */
 public class WordGames
 {
+    /**
+     * Adds "Hello" to the front of the input word.
+     *
+     * @param word The input word that will have "Hello" added to the front.
+     * @return A String containing "Hello" plus the input word.
+     */
     public String addHelloWord(String word)
     {
         return String.format("Hello %s", word);
     }
 
+    /**
+     * Concatenates a name and surname.
+     *
+     * @param name The first name.
+     * @param surname The last name.
+     * @return A String containing the full name.
+     */
     public String getFullName(String name, String surname)
     {
         return name + " " + surname;
